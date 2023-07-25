13ca458f51726fe6c8974c547eb1c294ad604ab6
sreejacb <101924446+sreejacb@users.noreply.github.com>
2023-07-25 18:46:58 +0530

    Update and rename appapi4.py to app_api.py

--- app_api.py
+++ app_api.py
@@ -6,7 +6,7 @@ url = "https://latest-stock-price.p.rapidapi.com/price"
 querystring = {"Indices":"NIFTY 50"}
 
 headers = {
-	"X-RapidAPI-Key": "a66b826b80msh4bb2388b0f46dddp1bd641jsn9f6a5ba0bee0",
+	"X-RapidAPI-Key": "<provide you api key>",
 	"X-RapidAPI-Host": "latest-stock-price.p.rapidapi.com"
 }
 
